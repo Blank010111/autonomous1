aa0e3bc613682e171e3f23f04f05e86e74e238bc
Blank010111 <blank32702@gmail.com>
2016-06-12 22:20:56 -0400

    Update canvas.js

--- javascripts/canvas.js
+++ javascripts/canvas.js
@@ -1,7 +1,4 @@
-var x = document.createElement("CANVAS");
-var x = document.getElementById("myCanvas");
-<canvas id="myCanvas" width="200" height="100" style="border:1px solid #000000;">
-</canvas>
+
 var board = createBoard(380,380);
    boardx = (windowWidth - width) / 2;
    boardy = ((windowHeight - height) / 2) + 100;
