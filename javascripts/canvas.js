626e83df53cbb32473e70c75e14d8601ab4948d9
Blank010111 <blank32702@gmail.com>
2016-06-12 22:53:38 -0400

    Update canvas.js

--- javascripts/canvas.js
+++ javascripts/canvas.js
@@ -1,8 +1,5 @@
 function setup() { 
-var centerx = (windowWidth - width) / 2;
- 	 var centery = (windowHeight - height) / 2;
-   var board = createBoard(380,380);
-   boardx = (windowWidth - width) / 2;
-   boardy = ((windowHeight - height) / 2) + 100;
-   board.position(boardx, boardy);
+function setup() {
+  cnv = createCanvas(600,600,WEBGL);
+  cnv.position((windowWidth-width)/2,(windowHeight-height)/2);
 };
