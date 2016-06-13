c3377f4815b17fae14ae2ad68aff0e67f7c26ec2
Blank010111 <blank32702@gmail.com>
2016-06-12 22:49:35 -0400

    Update canvas.js

--- javascripts/canvas.js
+++ javascripts/canvas.js
@@ -1,5 +1,8 @@
-
-var board = createBoard(380,380);
+function setup() { 
+var centerx = (windowWidth - width) / 2;
+ 	 var centery = (windowHeight - height) / 2;
+   var board = createBoard(380,380);
    boardx = (windowWidth - width) / 2;
    boardy = ((windowHeight - height) / 2) + 100;
    board.position(boardx, boardy);
+};
